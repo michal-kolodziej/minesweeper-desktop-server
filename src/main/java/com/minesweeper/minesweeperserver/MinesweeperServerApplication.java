9fe5cb71adbe409f93f45696077d438b907f4b4d
Artur Kuś <arturkus98@gmail.com>
2023-11-09 13:43:51 +0100

    Bootstrap application

---
+++ src/main/java/com/minesweeper/minesweeperserver/MinesweeperServerApplication.java
@@ -0,0 +1,13 @@
+package com.minesweeper.minesweeperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minesweeper/minesweeperserver/MinesweeperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minesweeper.minesweeperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
